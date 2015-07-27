7caaf46526ab149e120d6edbcf1f50062b95f603
javilonas <admin@lonasdigital.com>
2015-07-27 01:52:47 -0400

    cpufreq: Add new governor Barry-Allen [@Javilonas]
    Signed-off-by: placiano <placiano80@gmail.com>
    Signed-off-by: AudioGod <audiogod@sonic-developers.com>
    
    Conflicts:
    	drivers/cpufreq/Kconfig
    	drivers/cpufreq/Makefile
    
    Conflicts:
    	arch/arm64/configs/cm_oneplus2_defconfig
    	drivers/cpufreq/Kconfig
    	drivers/cpufreq/Makefile
    	include/linux/cpufreq.h
    
    Conflicts:
    	arch/arm64/configs/cm_oneplus2_defconfig

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -447,6 +447,9 @@ extern struct cpufreq_governor cpufreq_gov_impulse;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_DARKNESS)
 extern struct cpufreq_governor cpufreq_gov_darkness;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_darkness)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BARRY_ALLEN)
+extern struct cpufreq_governor cpufreq_gov_barry_allen;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_barry_allen)
 #endif
 
 /*********************************************************************
