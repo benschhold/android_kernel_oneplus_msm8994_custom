1e4d8a8186f179963ae03a78151341a57f9a1859
Pranav Vashi <neobuddy89@gmail.com>
2015-11-15 01:57:15 +0530

    drivers: state_notifier: Add toggle to enable driver
    * May required to debug SOD on some devices.
    * Also get rid of FB notifiers.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: AudioGod <audiogod@sonic-developers.com>

--- drivers/video/msm/mdss/mdss_dsi.c
+++ drivers/video/msm/mdss/mdss_dsi.c
@@ -1538,8 +1538,7 @@ static int mdss_dsi_event_handler(struct mdss_panel_data *pdata,
 			rc = mdss_dsi_unblank(pdata);
 		pdata->panel_info.esd_rdy = true;
 #ifdef CONFIG_STATE_NOTIFIER
-		if (!use_fb_notifier)
-			state_resume();
+		state_resume();
 #endif
 		break;
 	case MDSS_EVENT_BLANK:
@@ -1555,8 +1554,7 @@ static int mdss_dsi_event_handler(struct mdss_panel_data *pdata,
 			rc = mdss_dsi_blank(pdata, power_state);
 		rc = mdss_dsi_off(pdata, power_state);
 #ifdef CONFIG_STATE_NOTIFIER
-		if (!use_fb_notifier)
-			state_suspend();
+		state_suspend();
 #endif
 		break;
 	case MDSS_EVENT_CONT_SPLASH_FINISH:
