140f96d0208aceb5256d5422e53e0e9036d0efb6
benschhold <benschhold@gmail.com>
2016-08-05 13:16:16 +0200

    we want to save some battery, let just one core alive when screen is off
    Change-Id: I689164db9a2b4e3c7ba3a55195bc3544aa69ad99

--- arch/arm64/hotplug/thunderplug.c
+++ arch/arm64/hotplug/thunderplug.c
@@ -31,11 +31,11 @@
 #define DRIVER_VERSION                5
 #define DRIVER_SUBVER                 0
 
-#define DEFAULT_CPU_LOAD_THRESHOLD   (65)
+#define DEFAULT_CPU_LOAD_THRESHOLD   (45)
 #define MIN_CPU_LOAD_THRESHOLD       (10)
 
-#define HOTPLUG_ENABLED              (0)
-#define DEFAULT_HOTPLUG_STYLE         HOTPLUG_SCHED
+#define HOTPLUG_ENABLED              (1)
+#define DEFAULT_HOTPLUG_STYLE         HOTPLUG_PERCORE
 #define DEFAULT_SCHED_MODE            BALANCED
 
 #define DEF_SAMPLING_MS	             (500)
@@ -47,7 +47,7 @@ static bool isSuspended = false;
 
 struct notifier_block lcd_worker;
 
-static int suspend_cpu_num = 2, resume_cpu_num = (NR_CPUS -1);
+static int suspend_cpu_num = 1, resume_cpu_num = (NR_CPUS -1);
 static int endurance_level = 0;
 static int core_limit = NR_CPUS;
 
